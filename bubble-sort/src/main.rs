65823a409900c20951e70b55ee69af1bd11bf441
Frank Davies <fdavies93@gmail.com>
2023-05-21 23:07:26 +0800

    Updated bubble sort to remove extra lines

--- bubble-sort/src/main.rs
+++ bubble-sort/src/main.rs
@@ -1,12 +1,8 @@
 fn main() {
     let mut arr: [i32; 5] = [9, 4, 2, 7, 1];
-
-    let mut i1: usize = 0;
-    let mut i2: usize = 1;
-    let mut temp: i32 = 0;
+    let mut temp: i32;
 
     for i1 in 0..arr.len() {
-
         for i2 in i1+1..arr.len() {
             if arr[i1] > arr[i2] {
                 temp = arr[i1];
